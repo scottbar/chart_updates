73c4985b294d187a703538b739c9406180ebe32c
ScottB <4988821+scottbar@users.noreply.github.com>
2025-03-27 16:49:30 +0000

    Add indeed job data

--- update.py
+++ update.py
@@ -14,3 +14,84 @@ flourish_weekly = flourish.resample('W-SUN').bfill()
 flourish_weekly = flourish_weekly.assign(index_copy=flourish_weekly.index)
 flourish_weekly['Month'] = flourish_weekly['index_copy'].dt.strftime('%b %d, %Y')
 flourish_weekly.to_csv('output_w.csv')
+
+! pip install fredapi
+from fredapi import Fred
+indeed_codes = ['IHLIDXUSTPACCO',
+ 'IHLIDXUSTPADMIASSI',
+ 'IHLIDXUSTPARCH',
+ 'IHLIDXUSTPAREN',
+ 'IHLIDXUSTPBAFI',
+ 'IHLIDXUSTPBEWE',
+ 'IHLIDXUSTPCHIL',
+ 'IHLIDXUSTPCIVIENGI',
+ 'IHLIDXUSTPCLSA',
+ 'IHLIDXUSTPCONS',
+ 'IHLIDXUSTPCOSOSE',
+ 'IHLIDXUSTPCUSTSERV',
+ 'IHLIDXUSTPDENT',
+ 'IHLIDXUSTPDRIV',
+ 'IHLIDXUSTPEDIN',
+ 'IHLIDXUSTPELECENGI',
+ 'IHLIDXUSTPFOPRSE',
+ 'IHLIDXUSTPHOTO',
+ 'IHLIDXUSTPHUMARESO',
+ 'IHLIDXUSTPINDEDO',
+ 'IHLIDXUSTPINDUENGI',
+ 'IHLIDXUSTPINMA',
+ 'IHLIDXUSTPINSU',
+ 'IHLIDXUSTPITOPHE',
+ 'IHLIDXUSTPLEGA',
+ 'IHLIDXUSTPLOGISUPP',
+ 'IHLIDXUSTPLOST',
+ 'IHLIDXUSTPMANA',
+ 'IHLIDXUSTPMARK',
+ 'IHLIDXUSTPMATH',
+ 'IHLIDXUSTPMECO',
+ 'IHLIDXUSTPMEDIINFO',
+ 'IHLIDXUSTPMEDITECH',
+ 'IHLIDXUSTPNURS',
+ 'IHLIDXUSTPPECAHOHE',
+ 'IHLIDXUSTPPHAR',
+ 'IHLIDXUSTPPHSU',
+ 'IHLIDXUSTPPRMA',
+ 'IHLIDXUSTPPROJMANA',
+ 'IHLIDXUSTPRETA',
+ 'IHLIDXUSTPSALE',
+ 'IHLIDXUSTPSCREDE',
+ 'IHLIDXUSTPSEPUSA',
+ 'IHLIDXUSTPSOFTDEVE',
+ 'IHLIDXUSTPSPOR',
+ 'IHLIDXUSTPTHER',
+ 'IHLIDXUSTPVETE']
+
+import os
+fred_api_key = os.getenv('FRED_API')
+if fred_api_key is None:
+    raise ValueError("API_KEY environment variable is not set")
+
+fred = Fred(api_key=fred_api_key)
+col_names = pd.DataFrame({series:fred.get_series_info(series) for series in indeed_codes})
+col_names.head()
+collist = col_names.iloc[3].tolist()
+new_list = []
+
+for item in collist:
+  new_item = item.replace(" Job Postings on Indeed in the United States", "")
+  new_list.append(new_item)
+data = pd.DataFrame({series:fred.get_series(series) for series in indeed_codes})
+data.columns = new_list
+dataOut = data.copy()
+dataOut['Month'] = data.index
+
+
+# Convert the column to datetime format
+dataOut['Month'] = pd.to_datetime(dataOut['Month'])
+
+# Use strftime to format the date as 'Month-Year'
+dataOut['Month'] = dataOut['Month'].dt.strftime('%b-%y')
+# If needed select time period
+#dataOut = dataOut.loc['2001-01-01':]
+dataOut= dataOut.fillna(method='ffill')
+dataOut = dataOut.resample('M').last()
+dataOut.to_csv('indeed.csv')
