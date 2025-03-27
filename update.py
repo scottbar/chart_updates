a653e1163c161058d6ef1dc955837661a2a86ca4
ScottB <4988821+scottbar@users.noreply.github.com>
2025-03-27 17:00:30 +0000

    temp remove portwatch

--- update.py
+++ update.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import datetime as dt
 # Portwatch
+"""
 data = pd.read_csv('https://opendata.arcgis.com/api/v3/datasets/42132aa4e2fc4d41bdaf9a445f688931_0/downloads/data?format=csv&spatialRefId=4326&where=1=1')
 data['datetime'] = pd.to_datetime(data['date'], format='mixed')
 data['datetime'] = data['datetime'].dt.strftime('%Y-%m-%d')
@@ -14,7 +15,7 @@ flourish_weekly = flourish.resample('W-SUN').bfill()
 flourish_weekly = flourish_weekly.assign(index_copy=flourish_weekly.index)
 flourish_weekly['Month'] = flourish_weekly['index_copy'].dt.strftime('%b %d, %Y')
 flourish_weekly.to_csv('output_w.csv')
-
+"""
 from fredapi import Fred
 indeed_codes = ['IHLIDXUSTPACCO',
  'IHLIDXUSTPADMIASSI',
